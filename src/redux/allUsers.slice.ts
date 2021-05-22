22fd841f00312412552a3a7b166da2b6224e2ecc
jeffascript <ofoaro.jeff@gmail.com>
2021-05-22 18:31:20 +0200

    get all users with posts done

--- src/redux/allUsers.slice.ts
+++ src/redux/allUsers.slice.ts
@@ -4,7 +4,7 @@ import {
   SerializedError,
 } from "@reduxjs/toolkit";
 import firebase from "firebase";
-import { AppDispatch } from "./store";
+import { AppDispatch, RootState } from "./store";
 
 import { UserData } from "./user.slice";
 
@@ -15,7 +15,8 @@ interface Posts {
 }
 
 interface IAllUsers extends UserData {
-  posts: Posts[];
+  posts?: Posts[];
+  postError?: string | null | SerializedError;
 }
 
 type UserState = {
@@ -50,11 +51,17 @@ export const fetchAllUsers = () => {
       .collection("users")
       .onSnapshot(
         (snapshot) => {
-          dispatch(
-            fetchUsersSuccess(
-              snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-            )
-          );
+          const uids = snapshot.docs.map((doc) => doc.id);
+
+          for (let oneUid of uids) {
+            dispatch(fetchUsersData(oneUid));
+          }
+
+          //   dispatch(
+          //     fetchUsersSuccess(
+          //       snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+          //     )
+          //   );
         },
         (error) => {
           dispatch(fetchUsersFailure(error));
@@ -69,6 +76,97 @@ export const fetchAllUsers = () => {
  * foreach of the ids above--> attach a fetchUserWithData from
  * above will be attached with fetchUsersPosts in order to get the posts for that user
  */
+
+export const fetchUsersData = createAsyncThunk(
+  "users/fetchData",
+  async (uid: string, thunkAPI) => {
+    try {
+      const rootState = thunkAPI.getState() as RootState;
+      const found = rootState.allUsers.users.some((el) => el.uid === uid);
+      if (!found) {
+        const promise = await firebase
+          .firestore()
+          .collection("users")
+          .doc(uid)
+          .get()
+          .then((snapshot) => {
+            if (snapshot.exists !== true) {
+              console.log("does not exist");
+            }
+            // console.log(snapshot.data());
+            let user = snapshot.data(); //Method for Retrieving a single document from Cloud Firestore
+            user.uid = snapshot.id;
+            thunkAPI.dispatch(fetchUsersPosts(user.uid)); //setting id to the function redux action creator below
+            return user;
+          });
+
+        thunkAPI.dispatch(fetchUsersSuccess(promise));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+/**
+ * fetch all Users id in the collection with onSnapshot,
+ * foreach of the ids above--> attach a fetchUserWithData from
+ * above will be attached with fetchUsersPosts in order to get the posts for that user
+ */
+
+export const fetchUsersPosts = createAsyncThunk(
+  "users/posts",
+  async (uid: string, { rejectWithValue, ...thunkAPI }) => {
+    console.log({ uidQuery: uid });
+    try {
+      const promise = await firebase
+        .firestore()
+        .collection("posts")
+        .doc(uid)
+        .collection("userPosts")
+        .orderBy("creation", "asc")
+        .get()
+        .then((snapshot: any) => {
+          console.log({ snapshot });
+
+          //cf: https://stackoverflow.com/questions/38731620/firebase-query-based-on-child-prop-value-and-get-path
+          //   const uid = snapshot.query.Hf.path.segments[1];//this is an array containing each step of the path. so if the actual path is for example foo/bar/baz, this will show as ["foo","bar","baz"], which obviously you can read out with
+          const uid: any = snapshot._delegate.query._query.T.path.segments[1]; // I had to inspect resolved snapshot above to get this value
+
+          console.log({ uid });
+          const rootState = thunkAPI.getState() as RootState;
+          const user = rootState.allUsers.users.find((el) => el.uid === uid);
+
+          // console.log(snapshot.docs[0].data());
+          // Querying a collection from Cloud Firestore (doc => collection => field) //map the docs and do a get data method for each ===> .data()
+          const response = snapshot.docs.map((doc) => {
+            const data = doc.data();
+            const id = doc.id;
+            return { id, ...data, user };
+          });
+
+          //   console.log("fetchUsersFollowingPosts => posts", response);
+
+          //   console.log(
+          //     "fetchUsersFollowingPosts => getState",
+          //     thunkAPI.getState()
+          //   );
+
+          return response;
+          // if (snapshot.exists) {
+          //   console.log({ thunkAPI });
+          //   console.log(snapshot.docs);
+          //   return snapshot.data();
+          // }
+        });
+      thunkAPI.dispatch(fetchUsersPostsSuccess(promise)); // action for the reducer dispatched here to fulfilled
+    } catch (err) {
+      console.log(err);
+      const error = rejectWithValue(err);
+      // thunkAPI.dispatch(fetchUsersPostsFailure(error)); // action for the reducer dispatched here to rejected
+    }
+  }
+);
 
 const initialState = {
   users: [],
@@ -87,13 +185,38 @@ const allUsers = createSlice({
     },
 
     fetchUsersSuccess: (state, action) => {
-      state.usersLoadedCount = state.usersLoadedCount + 1; //increment the numbers by 1 everytime new user was added
       state.users.push(action.payload);
       state.loadingUsersStatus = "idle";
     },
     fetchUsersFailure: (state, action) => {
       if (action.payload) state.usersDataError = action.payload;
       state.loadingUsersStatus = "idle";
+    },
+
+    fetchUsersPostsSuccess: (state, action) => {
+      state.usersLoadedCount = state.usersLoadedCount + 1; //increment the numbers by 1 everytime new user was added
+      // state.users.push(action.payload);
+      state.loadingUsersStatus = "idle";
+
+      if (action.payload.length > 0) {
+        state.users = state.users.map((user) =>
+          user.uid === action.payload[0].user.uid
+            ? { ...user, posts: action.payload }
+            : { ...user, posts: [] }
+        );
+      }
+    },
+
+    fetchUsersPostsFailure: (state, action) => {
+      state.loadingUsersStatus = "idle";
+
+      if (action.payload.length > 0) {
+        state.users = state.users.map((user) =>
+          user.uid === action.payload[0].user.uid
+            ? { ...user, postError: action.payload }
+            : { ...user, postError: null }
+        );
+      }
     },
 
     //   clearUserData: (state) => {
@@ -105,10 +228,15 @@ const allUsers = createSlice({
     //     };
     //   },
   },
-  extraReducers: () => {},
+  //   extraReducers: () => {},
 });
 
-export const { fetchUsersPending, fetchUsersFailure, fetchUsersSuccess } =
-  allUsers.actions;
+export const {
+  fetchUsersPending,
+  fetchUsersFailure,
+  fetchUsersSuccess,
+  fetchUsersPostsSuccess,
+  fetchUsersPostsFailure,
+} = allUsers.actions;
 
 export default allUsers.reducer;
